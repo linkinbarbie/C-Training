3a0e0dd30c364fa3ffc73edb399c86f159cb04d3
linkinbarbie <linkinbarbie@gmail.com>
2020-05-26 22:53:48 +0100

    Sum Average

---
+++ Sum_Average/Sum_Average.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Sum_Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("Please enter your number");
+
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter your second number");
+
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            //sum
+            int num_sum = num1 + num2;
+            Console.WriteLine("The sum of your numbers is {0}", num_sum);
+
+            int num_ave = (num1 + num2) / 2;
+            Console.WriteLine("The average of your numbers is {0}", num_ave);
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
